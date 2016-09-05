5cccdc346a16ed843d9eb7529e3f74bd7b58f602
Janos Szathmary <szathmary.janos89@gmail.com>
2016-09-05 09:52:42 +0200

    logoutuser feature added

--- app.frontend/dev/src/js/reducers/index.js
+++ app.frontend/dev/src/js/reducers/index.js
@@ -1,8 +1,34 @@
 import { combineReducers } from "redux"
 import { routerStateReducer } from "redux-router";
 import user from "./userReducer"
+import error from "./errorReducer"
 
+/*
 export default combineReducers({
+	error,
     user,
     router: routerStateReducer
 })
+*/
+
+// Based on this:
+// http://stackoverflow.com/questions/35622588/how-to-reset-the-state-of-a-redux-store/35641992#35641992
+const appReducer = combineReducers({
+    error,
+    user,
+    router: routerStateReducer
+})
+
+const rootReducer = (state, action) => {
+    if (action.type === "LOGOUT_USER") {
+        localStorage.removeItem("token");
+        // User voiud 0 instead of undefined
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/void
+        // http://eslint.org/docs/rules/no-undefined
+        state = void 0;
+    }
+
+    return appReducer(state, action)
+}
+
+export default rootReducer;
