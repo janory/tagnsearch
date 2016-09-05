641a4c46f9dddee2267ec6e69eacebcba200ebb9
Janos Szathmary <szathmary.janos89@gmail.com>
2016-09-05 09:53:09 +0200

    user registration added

--- app.frontend/dev/src/js/routes/index.js
+++ app.frontend/dev/src/js/routes/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, IndexRedirect } from "react-router";
 import App from "../containers/App";
-import { LoginView, WelcomeView, NotFoundView } from "../views";
+import { LoginView, RegistrationView, WelcomeView, NotFoundView } from "../views";
 import { requireAuthentication } from "../utils";
 
 export default (
     <Route path="/" component={App}>
         <IndexRedirect to="/login" />
         <Route path="/login" component={LoginView}></Route>
+        <Route path="/registration" component={RegistrationView}></Route>
         <Route path="/welcome" component={requireAuthentication(WelcomeView)} />
         <Route path="*" component={NotFoundView} />
     </Route>
