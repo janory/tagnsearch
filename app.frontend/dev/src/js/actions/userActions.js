220f75dec999a052f499b4f88e53389c1df65633
Janos Szathmary <szathmary.janos89@gmail.com>
2016-09-05 10:12:08 +0200

    user registration added

--- app.frontend/dev/src/js/actions/userActions.js
+++ app.frontend/dev/src/js/actions/userActions.js
@@ -14,6 +14,32 @@ export function loginUser(username, password, redirectTo) {
             dispatch(push(redirectTo))
         }).catch((err) => {
             dispatch({type: "LOGIN_USER_REJECTED", payload: err})
+            dispatch({type: "ERROR_SET", payload: err})
         })
+    }
+}
+
+export function registrateUser(user) {
+    return dispatch => {
+        dispatch({type: "REGISTRATE_USER", payload: ""})
+        axios.put("api/registration", {
+            username: user.get("username"),
+            password: md5(user.get("password")),
+            firstName: user.get("firstName"),
+            lastName: user.get("lastName")
+        })
+        .then((response) => {
+            dispatch({type: "REGISTRATE_USER_FULFILLED", payload: response})
+            dispatch(push("/login"))
+        }).catch((err) => {
+            dispatch({type: "ERROR_SET", payload: err})
+        })
+    }
+}
+
+export function logoutUser() {
+     return dispatch => {
+        dispatch({type: "LOGOUT_USER", payload: ""})
+        dispatch(push("/login"))
     }
 }
