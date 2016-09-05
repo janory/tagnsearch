431e0d1fa7a3edbdbf8ac29452196c55201c4838
Janos Szathmary <szathmary.janos89@gmail.com>
2016-09-05 09:51:51 +0200

    basic error handling; logoutuser feature added

--- app.frontend/dev/src/js/containers/App.js
+++ app.frontend/dev/src/js/containers/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Error from "../components/Error";
 import { Container } from "semantic-react";
 import { connect } from "react-redux"
+import { clearError } from "../actions/errorActions"
+import { logoutUser } from "../actions/userActions"
 
 @connect((state) => {
     return {
-        isAuthenticated: state.user.isAuthenticated
+        isAuthenticated: state.user.isAuthenticated,
+        error: state.error
     };
 })
 export default class App extends Component {
+
+    clearError() {
+        this.props.dispatch(clearError());
+    }
+
+    logoutUser() {
+        this.props.dispatch(logoutUser());
+    }
+
     render() {
         return (
             <div>
-                { this.props.isAuthenticated ? <Header/> : "" }
+                { this.props.isAuthenticated ? <Header logoutUser={this.logoutUser.bind(this)} /> : "" }
                 <Container>{this.props.children}</Container>
                 <Footer/>
+                <Error clearError={this.clearError.bind(this)} error={this.props.error} />
             </div>
         );
     }
