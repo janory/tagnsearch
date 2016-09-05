568c9d519b10fbee49ebc28d82bdd0926d9fd6f2
Janos Szathmary <szathmary.janos89@gmail.com>
2016-09-05 09:52:15 +0200

    basic error handling; logoutuser feature added

--- app.frontend/dev/src/js/components/Header.js
+++ app.frontend/dev/src/js/components/Header.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import { Menu, MenuItem, Container } from "semantic-react";
 
 export default class Header extends Component {
+
+    logout() {
+        this.props.logoutUser();
+    }
+
     render() {
         return (
             <Container>
-                <Menu>
-                    <MenuItem>First item</MenuItem>
-                    <MenuItem>Second Item</MenuItem>
+                <Menu borderless>
+                    <Menu>
+                        <MenuItem>First item</MenuItem>
+                        <MenuItem>Second Item</MenuItem>
+                    </Menu>
+                    <Menu floated="right">
+                        <MenuItem onClick={this.logout.bind(this)}>Logout</MenuItem>
+                    </Menu>
                 </Menu>
             </Container>
         );
